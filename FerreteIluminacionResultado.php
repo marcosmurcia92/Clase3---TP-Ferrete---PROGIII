72f28650f35d85b5507a931b3b02c0ec0400d8a0
Marcos Facundo Murcia <marcosmurcia92@gmail.com>
2016-04-03 20:16:52 -0300

    Finalizado Ferrete Iluminacion

--- FerreteIluminacionResultado.php
+++ FerreteIluminacionResultado.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Ferrete Facturacion Resultado</title>
+	<title>Ferrete Iluminacion Resultado</title>
 	<link rel="stylesheet" href="estilo.css">
 	<link rel="stylesheet" href="animacion.css">
 </head>
@@ -9,22 +9,46 @@
 		<form class="CajaEnunciado">
 			<?php
 				//var_dump($_POST);
-				$primerProd = $_POST['Prod1'];
-				$segundoProd = $_POST['Prod2'];
-				$tercerProd = $_POST['Prod3'];
-				if(isset($_POST['Sumar'])){
-					$suma = $primerProd + $segundoProd + $tercerProd;
-					echo "La suma de los tres productos es de: ".$suma;
-				}else if(isset($_POST['Promediar'])){
-					$prom = ($primerProd + $segundoProd + $tercerProd) / 3;
-					echo "El promedio de los tres precios es de: ".$prom;
-				}else if(isset($_POST['PrecioFinal'])){
-					$precioFin = ($primerProd + $segundoProd + $tercerProd) * 1.21;
-					echo "El precio final (+IVA 21%) es : ".$precioFin;
+				$cantidad = $_POST['Cantidad'];
+				$marca = $_POST['Marca'];
+				$precio = $cantidad * 35;
+				$iibb = 0;
+				if($cantidad >= 6){
+					$precio = $precio * 0.5;
+				}else if($cantidad == 5){
+					if($marca == "ArgentinaLuz"){
+						$precio = $precio * 0.6;
+					}else{
+						$precio = $precio * 0.7;
+					}
+				}else if($cantidad == 4){
+					if($marca == "ArgentinaLuz" || $marca == "FelipeLamparas"){
+						$precio = $precio * 0.75;
+					}else{
+						$precio = $precio * 0.8;
+					}
+				}else if($cantidad == 3){
+					if($marca == "ArgentinaLuz"){
+						$precio = $precio * 0.85;
+					}else if($marca == "FelipeLamparas"){
+						$precio = $precio * 0.9;
+					}else{
+						$precio = $precio * 0.95;
+					}
 				}
+
+				if($precio > 120){
+					$iibb = $precio * 0.1;
+					$precio = $precio * 1.1;
+				}				
+
+				if($iibb > 0){
+					echo "Usted debe pagar $".$iibb." por impuestos a ingresos brutos.<br>";
+				}
+				echo "El total a pagar es de $".$precio;
 			?> 
 		</form><br><br><br><br><br>
-		<form action="FerreteFacturacion.html" method="POST">
+		<form action="FerreteIluminacion.html" method="POST">
 			<input type="Submit" class = "MiBotonUTN" value="Volver Atras">
 		</form>
 	</div>
